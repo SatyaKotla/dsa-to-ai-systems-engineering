9653faaa9b76f12af1135e5bea88c1406bbdb30f
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-02-26 14:02:51 -0500

    Enhance DynamicArray: add reverse, encapsulation (refactor internal variables) and dunder(double underscore) methods

--- 01_core_dsa/arrays/dynamic_array.py
+++ 01_core_dsa/arrays/dynamic_array.py
@@ -1,113 +1,150 @@
 #dynamic array 
 class DynamicArray():
     def __init__(self):
-        self.max_len = 1
-        self.size = 0
-        self.array = [None] * self.max_len
+        self._max_len = 1 # capacity of the array
+        self._size = 0 # actual size of the array
+        self._array = [None] * self._max_len # array
 
     def append(self, item):
-        if self.size == self.max_len:
+        if self._size == self._max_len:
             self._resize()
 
-        self.array[self.size] = item 
-        self.size += 1
+        self._array[self._size] = item 
+        self._size += 1
     
     def _resize(self):
-        self.max_len = self.max_len * 2 
-        new_array = [None] * self.max_len
+        self._max_len = self._max_len * 2 
+        new_array = [None] * self._max_len
         
-        for i in range(self.size):
-            new_array[i] = self.array[i]
+        for i in range(self._size):
+            new_array[i] = self._array[i]
         
-        self.array = new_array
+        self._array = new_array
 
     def __len__(self):
-        return self.size
+        return self._size
     
     def __getitem__(self, index):
         if index < 0:
-            index = self.size + index 
+            index = self._size + index 
         
-        if index < 0 or index >= self.size:
+        if index < 0 or index >= self._size:
             raise IndexError("Index is out of range")
         
-        return self.array[index]
+        return self._array[index]
+    
+    # to access the element to set value to at the index position
+    def __setitem__(self, index, value):
+        if index < 0:
+            index = self._size + index
+
+        if index < 0 or index >= self._size:
+            raise IndexError("Index is out of range")
+        
+        self._array[index] = value
+    
+    # for looping the elements directly
+    def __iter__(self):
+        for i in range(self._size):
+            yield self._array[i]
+    
+    # to get the array without the other memory values
+    def to_list(self):
+        return self._array[:self._size]
     
     def insert(self, index,value):
         # Handling negative index
         if index < 0:
-            index = self.size + index
+            index = self._size + index
 
         # Allowing insertion at the end (index == self.size)
-        if index < 0 or index > self.size:
+        if index < 0 or index > self._size:
             raise IndexError("Index is out of range")
         
         #Resize if full
-        if self.size == self.max_len:
+        if self._size == self._max_len:
                 self._resize()
-        for i in range(self.size, index, -1):
-            self.array[i] = self.array[i - 1]
+        for i in range(self._size, index, -1):
+            self._array[i] = self._array[i - 1]
 
         #Insertion
-        self.array[index] = value
-        self.size = self.size + 1
+        self._array[index] = value
+        self._size = self._size + 1
 
     def pop(self):
-        if self.size == 0:
+        if self._size == 0:
             raise IndexError("Popping from an empty array.")
-        value = self.array[self.size - 1]
-        self.array[self.size - 1] = None 
-        self.size = self.size - 1
+        value = self._array[self._size - 1]
+        self._array[self._size - 1] = None 
+        self._size = self._size - 1
 
         # shrink the capacity of the array if size is less than 
         # 1/4th of the capacity
-        if self.size > 0 and self.size <= self.max_len //4:
+        if self._size > 0 and self._size <= self._max_len //4:
             self._shrink()
         return value 
     
     
     def _shrink(self):
-        self.max_len = self.max_len // 2
-        new_array = [None] * self.max_len 
+        self._max_len = self._max_len // 2
+        new_array = [None] * self._max_len 
 
-        for i in range(self.size):
-            new_array[i] = self.array[i]
+        for i in range(self._size):
+            new_array[i] = self._array[i]
         
-        self.array =  new_array 
+        self._array =  new_array 
     
     def delete(self, index):
         if index < 0:
-            index = self.size + index
+            index = self._size + index
         
-        if index < 0 and index >= self.size:
+        if index < 0 and index >= self._size:
             raise IndexError("Index is out of range.")
         
-        value = self.array[index]
+        value = self._array[index]
 
         #shift left
-        for i in range(index, self.size-1):
-            self.array[i] = self.array[i + 1]
+        for i in range(index, self._size-1):
+            self._array[i] = self._array[i + 1]
         
-        self.array[self.size -1] = None 
-        self.size = self.size - 1
+        self._array[self._size -1] = None 
+        self._size = self._size - 1
 
-        if self.size > 0 and self.size <= self.max_len//4 :
+        if self._size > 0 and self._size <= self._max_len//4 :
             self._shrink()
         
         return (value)
+    
+    # Reverse an Array (In - Place)
+    """
+    Reverse elements without creating a new array.
+    For example:
+    Input: [1, 2, 3, 4]
+    Output: [4, 3, 2, 1]
+
+    """
+    def reverse(self):
+        i = 0
+        while i < self._size // 2:
+            (self._array[i], self._array[self._size-1]) = (self._array[self._size-1], 
+                                                self._array[i])
+            i = i+1
+        return self._array 
 
 
 
 if __name__ == "__main__":
     a = DynamicArray()
-    print(f"Initial array {a.array}")
+    print(f"Initial array {a.to_list()}")
     a.append(10)
     a.append(20)
     a.append(30)
-    print(f"array after appending 10, 20 , 30 : {a.array[:a.size]}")
+    print(f"\nArray after appending 10, 20 , 30 : {a.to_list()}")
     a.insert(1, 40)
-    print(f"array after inserting an element at index 1 :{a.array[:a.size]}")
+    print(f"\nArray after inserting an element at index 1 :{a.to_list()}")
     popped_value = a.pop()
-    print(f"popped value is :{popped_value} ; array after popping is : {a.array[:a.size]}")
+    print(f"\nPopped value is :{popped_value}; array after popping is : {a.to_list()}")
     deleted_value = a.delete(0)
-    print(f"deleted value is : {deleted_value}; array after deleting an element at 0th index : {a.array[:a.size]}")
+    print(f"\nDeleted value is : {deleted_value}; array after deleting an element at 0th index : {a.to_list()}")
+    a.reverse()
+    print(f"\nArray after reversing it in place: {a.to_list()}")
