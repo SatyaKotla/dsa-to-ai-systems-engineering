660d41a744b0a282fc160ca3eb7b1841adb75dae
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-01 14:42:35 -0500

    feat(algorithms): add prefixsum solution for Continuous Subarray Sum (O(n))

--- core_dsa/algorithms/prefix_sum.py
+++ core_dsa/algorithms/prefix_sum.py
@@ -18,10 +18,30 @@ def subarray_sum_equals_k(nums: list[int], k: int) -> int :
     
     return count
 
+# Continous Subarray Sum
+"""
+Problem: Given a list nums and and integer k, return True if there
+exists a subarray of size >= 2 whose sum is a multiple of k. 
+"""
+def subarray_sum_multiples_of_k(nums: list[int], k: int) -> bool:
+    prefix_sum = 0 
+    seen = {0:-1} # remainder: first index where it appeared
+
+    for i, num in enumerate(nums):
+        prefix_sum = prefix_sum + num 
+        remainder = prefix_sum % k if k!=0 else prefix_sum # to avoid zero division
+
+        if remainder in seen:
+            if i - seen[remainder] > 1:
+                return True 
+        else:
+            seen[remainder] = i 
+    return False
+
 def main():
-    nums = [1, 1, 1]
+    nums = [1, -1, 1,1]
     k = 2
-    print(subarray_sum_equals_k(nums, k))
+    print(subarray_sum_multiples_of_k(nums, k))
 
 if __name__ == "__main__":
     main()
