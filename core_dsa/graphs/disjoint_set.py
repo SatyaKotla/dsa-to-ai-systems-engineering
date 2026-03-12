ed7bee805492c4676e370e069e6eac798c353c36
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-12 17:45:37 -0400

    perf(graph): optimize Disjoint Set with path compression and union by rank

--- core_dsa/graphs/disjoint_set.py
+++ core_dsa/graphs/disjoint_set.py
@@ -6,22 +6,38 @@ class DijointSet:
         - make_set
         - find
         - union
+
+    Optimizations:
+        - Path Compression for find
+        - Union by Rank
     """
 
     def __init__(self):
         self.parent = {}
+        self.rank = {}
 
     def make_set(self, x):
         self.parent[x] = x
+        self.rank[x] = 0
 
     def find(self, x):
-        while self.parent[x] != x:
-            x = self.parent[x]
-        return x
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
 
-        if root_x != root_y:
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+
+        elif self.rank[root_x] > self.rank[root_y]:
             self.parent[root_y] = root_x
+
+        else:
+            self.parent[root_y] = root_x
+            self.rank[root_x] += 1
