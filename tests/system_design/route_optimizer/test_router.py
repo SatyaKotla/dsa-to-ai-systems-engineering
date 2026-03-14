8473f7c6020f58eea0405c64004453e27c76cd01
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-14 16:58:53 -0400

    test(route-optimizer): add unit tests for routing engine
    - Added tests for compute_route
    - Covered reachable routes
    - Covered unreachable routes
    - Covered same start and goal scenarios

---
+++ tests/system_design/route_optimizer/test_router.py
@@ -0,0 +1,71 @@
+####################################################
+# --------- TESTS FOR ROUTER COMPONENT -------- ####
+####################################################
+from system_design.route_optimizer.engine.router import compute_route
+from core_dsa.graphs.adjacency_list import Graph
+
+# Test the basic route
+
+
+def build_test_graph():
+
+    g = Graph(directed=True)
+
+    g.add_edge("A", "B", 1)
+    g.add_edge("B", "C", 1)
+
+    g.coords["A"] = (0, 0)
+    g.coords["B"] = (1, 0)
+    g.coords["C"] = (2, 0)
+
+    return g
+
+
+def test_compute_route_basic():
+
+    g = build_test_graph()
+
+    start = (0.1, 0)
+    goal = (1.9, 0)
+
+    path, distance = compute_route(g, start, goal)
+
+    assert path == ["A", "B", "C"]
+    assert distance == 2
+
+
+# Test: Unreachable Node
+def test_compute_route_unreachable():
+
+    g = Graph(directed=True)
+
+    g.add_edge("A", "B", 1)
+    g.add_vertex("C")
+
+    g.coords["A"] = (0, 0)
+    g.coords["B"] = (1, 0)
+    g.coords["C"] = (5, 5)
+
+    start = (0, 0)
+    goal = (5, 5)
+
+    path, distance = compute_route(g, start, goal)
+
+    assert path is None
+    assert distance == float("inf")
+
+
+# Test for Same Start and Goal
+
+
+def test_compute_route_same_node():
+
+    g = build_test_graph()
+
+    start = (0.1, 0)
+    goal = (0.2, 0)
+
+    path, distance = compute_route(g, start, goal)
+
+    assert path == ["A"]
+    assert distance == 0
