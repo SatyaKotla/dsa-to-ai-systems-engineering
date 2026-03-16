e9a5eb2e4c4f6cfdb6250aea68647f2466d4c28f
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-16 13:16:35 -0400

    feat(map-loader): integrate edge metadata and cost model for computation
    - Integrated EdgeMetadata class and cost-model interface for weight
      computation in the Map Loader.
    - Modified the MapLoader class to support legacy edge list format and
      metadata schema.

--- system_design/route_optimizer/loaders/map_loader.py
+++ system_design/route_optimizer/loaders/map_loader.py
@@ -1,11 +1,16 @@
 import json
 from core_dsa.graphs.adjacency_list import Graph
+from ..engine.edge_metadata import EdgeMetadata
+from ..engine.cost_models.distance_cost import DistanceCost
 
 
 class MapLoader:
 
+    def __init__(self, cost_model=None):
+        self.cost_model = cost_model or DistanceCost()
+
     @staticmethod
-    def from_json(file_path):
+    def from_json(self, file_path):
 
         with open(file_path, "r") as f:
             data = json.load(f)
@@ -17,7 +22,25 @@ class MapLoader:
             graph.add_vertex(node, coordinates)
 
         # add edges
-        for source, target, weight in data["edges"]:
-            graph.add_edge(source, target, weight)
+        for edge in data["edges"]:
+
+            # OLD FORMAT (grid graphs)
+            if isinstance(edge, list):
+                u, v, weight = edge
+                graph.add_edge(u, v, weight=weight)
+
+            # NEW FORMAT (metadata schema)
+            elif isinstance(edge, dict):
+                u = edge["from"]
+                v = edge["to"]
+
+                distance = edge.get("distance")
+                speed = edge.get("speed")
+
+                metadata = EdgeMetadata(distance=distance, speed=speed)
+
+                weight = self.cost_model.compute(metadata)
+
+                graph.add_edge(u, v, weight)
 
         return graph
