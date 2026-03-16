a0026af3e9694cece9e5464e36a6d4b5d7c4d3d2
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-16 00:30:10 -0400

    add RoutingService for routing orchestration

---
+++ system_design/route_optimizer/services/route_service.py
@@ -0,0 +1,20 @@
+from ..loaders.map_loader import MapLoader
+from ..engine.router import Router
+from ..engine.spatial_index_factory import create_spatial_index
+
+
+class RoutingService:
+
+    def __init__(self, map_path, spatial_method="kdtree"):
+
+        self.graph = MapLoader.from_json(map_path)
+
+        self.index = create_spatial_index(graph=self.graph, method=spatial_method)
+
+        self.router = Router(graph=self.graph, spatial_index=self.index)
+
+    def route(self, start_coordinates, goal_coordinates):
+
+        return self.router.compute_route(
+            start_coordinates=start_coordinates, goal_coordinates=goal_coordinates
+        )
