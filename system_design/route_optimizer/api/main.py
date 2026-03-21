e71bd529c49e25b959ea48b4a33ba2b2f73f99bb
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-21 10:59:34 -0400

    feat(frontend): add Leaflet-based UI with route visualization
    - Implemented interactive map using Leaflet
    - Added click-based start and end point selection
    - Integrated frontend with FastAPI routing API
    - Visualized routes using polyline
    - Added marker management and reset logic
    - Included console debugging for debugging during UI development

--- system_design/route_optimizer/api/main.py
+++ system_design/route_optimizer/api/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
 from system_design.route_optimizer.api.routes.route_api import router as route_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ButterBiscuit API 🍪", version="1.0")
 
 app.include_router(route_router)
+
+# accept frontend calls
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
