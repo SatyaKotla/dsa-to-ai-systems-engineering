027d379f113b90cd81adb618fd54aa4e0723aba1
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-03-30 16:05:29 -0400

    feat: add rate limiting to gateway and improve gateway structure
    - integrate slowapi limiter
    - limit /route endpoint to 10 requests per minute per IP
    - add rate limit exception handler
    - clean proxy_main structure

--- system_design/route_optimizer/api/proxy_main.py
+++ system_design/route_optimizer/api/proxy_main.py
@@ -1,22 +1,80 @@
+###################################
+# 1. Imports
+###################################
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi import Request
+
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
+import os
+import requests
+
 from system_design.route_optimizer.api.models.route_request import RouteRequest
 from system_design.route_optimizer.api.models.route_response import RouteResponse
-import requests
-import os
-from fastapi.middleware.cors import CORSMiddleware
 
+
+#################################
+# 2. App Initialization
+#################################
 app = FastAPI(title="ButterBiscuit Gateway 🍪", version="1.0")
 
+
+#################################
+# 3. Middleware
+#################################
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://route-optimizer-khaki.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+##################################
+# 4. Rate Limiter Setup
+##################################
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+
+app.add_middleware(SlowAPIMiddleware)
+
+
+###################################
+# 5. Exception Handlers
+###################################
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={"error": "Too many requests, Please try again later."},
+        headers={
+            "Access-Control-Allow-Origin": "https://route-optimizer-khaki.vercel.app",
+        },
+    )
+
+
+##################################
+# 6. Config
+##################################
 BACKEND_URL = "https://route-optimizer-api.onrender.com/route"
 API_KEY = os.getenv("API_KEY")
 
 
+####################################
+# 7. Routes
+####################################
 @app.post("/route", response_model=RouteResponse, response_model_exclude_none=True)
-async def proxy_route(request: RouteRequest):
+@limiter.limit("10/minute")  # rate limit
+async def proxy_route(request: Request, body: RouteRequest):
 
     response = requests.post(
         BACKEND_URL,
-        json=request.model_dump(),
+        json=body.model_dump(),
         headers={"x-api-key": API_KEY, "Content-Type": "application/json"},
         timeout=10,
     )
@@ -26,17 +84,9 @@ async def proxy_route(request: RouteRequest):
         raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
-# accept frontend calls
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://route-optimizer-khaki.vercel.app"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# health check
+#########################
+# 8. Health Check
+#########################
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
