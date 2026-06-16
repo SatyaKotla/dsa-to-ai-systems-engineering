f849a1928f6bfbc296640913ef24897d58b8583a
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-06-16 15:12:19 -0400

    feat(NLP): add configurable N-gram generator

---
+++ core_dsa/nlp/ngram_generator.py
@@ -0,0 +1,41 @@
+from core_dsa.arrays.dynamic_array import DynamicArray
+from core_dsa.nlp.tokenizer import ManualTokenizer
+
+
+class NGram:
+
+    def __init__(self, n: int):
+
+        if n <= 0:
+            raise ValueError("n must be greater than 0")
+
+        self.n = n
+
+    def generate(self, tokens: DynamicArray) -> DynamicArray:
+
+        n_grams = DynamicArray()
+
+        for start in range(len(tokens) - self.n + 1):
+            n_gram_temp = DynamicArray()
+            for i in range(self.n):
+                n_gram_temp.append(tokens[start + i])
+            n_grams.append(tuple(n_gram_temp))
+
+        return n_grams
+
+
+def main() -> None:
+    "Entry point for manual execution."
+
+    text = "Hello, world, hi, how are you "
+    tokenizer = ManualTokenizer()
+    tokens = tokenizer.tokenize(text)
+
+    bigram = NGram(2)
+    print(bigram.generate(tokens).to_list())
+    trigram = NGram(3)
+    print(trigram.generate(tokens).to_list())
+
+
+if __name__ == "__main__":
+    main()
