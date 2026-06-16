0d8decde83b4501b00d43c12e845daa8fb19bbce
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-06-16 18:00:47 -0400

    feat(NLP): add BPE pair frequency counting method

--- core_dsa/nlp/byte_pair_encoding.py
+++ core_dsa/nlp/byte_pair_encoding.py
@@ -1,4 +1,5 @@
 from core_dsa.arrays.dynamic_array import DynamicArray
+from core_dsa.nlp.ngram_generator import NGram
 
 
 class BPE:
@@ -11,15 +12,32 @@ class BPE:
 
         return tuple(symbols)
 
+    def get_pair_frequencies(self, symbols: tuple):
+
+        pair_frequencies = {}
+
+        bigram_generator = NGram(2)
+        pairs = bigram_generator.generate(symbols)
+
+        for pair in pairs:
+
+            if pair in pair_frequencies:
+                pair_frequencies[pair] += 1
+            else:
+                pair_frequencies[pair] = 1
+
+        return pair_frequencies
+
 
 def main() -> None:
     "Entry point for manual execution."
 
-    text = "hello"
+    text = "abab"
 
     bpe = BPE()
+    symbols = bpe.word_to_symbols(text)
 
-    print(bpe.word_to_symbols(text))
+    print(bpe.get_pair_frequencies(symbols))
 
 
 if __name__ == "__main__":
