af1a10aaa25e4e281d43fc2f28e3941100b195f7
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-06-19 00:48:32 -0400

    feat(NLP): initialize byte-level BPE vocabulary

--- core_dsa/nlp/byte_level_bpe.py
+++ core_dsa/nlp/byte_level_bpe.py
@@ -1,5 +1,14 @@
 class ByteLevelBPE:
 
+    def __init__(self):
+
+        self.token_to_id = {}
+        self.id_to_token = {}
+
+        for byte in range(256):
+            self.token_to_id[(byte,)] = byte
+            self.id_to_token[byte] = (byte,)
+
     def word_to_bytes(self, text: str) -> tuple:
 
         return tuple(text.encode("utf-8"))
@@ -10,13 +19,8 @@ def main() -> None:
 
     bpe = ByteLevelBPE()
 
-    word1 = "low"
-    word2 = "cafe"
-    word3 = "🚀"
-
-    print(f"{word1} in bytes is {bpe.word_to_bytes(word1)}")
-    print(f"{word2} in bytes is {bpe.word_to_bytes(word2)}")
-    print(f"{word3} in bytes is {bpe.word_to_bytes(word3)}")
+    print(bpe.id_to_token[108])
+    print(bpe.token_to_id[(108,)])
 
 
 if __name__ == "__main__":
