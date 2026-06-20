84357bafb8cadc537f34eeb10b9e290ff78d4562
SatyaKotla <96969946+SatyaKotla@users.noreply.github.com>
2026-06-20 01:31:37 -0400

    test(nlp): add unit tests for byte level bpe

---
+++ tests/core_dsa/nlp/test_byte_level_bpe.py
@@ -0,0 +1,75 @@
+####################################################
+# ------- TESTS FOR BYTE LEVEL BPE ------- ######
+####################################################
+from core_dsa.nlp.byte_level_bpe import ByteLevelBPE
+from core_dsa.arrays.dynamic_array import DynamicArray
+
+# Test Save/Load Round Trip
+
+
+def test_save_load_roundtrip():
+    words = DynamicArray()
+
+    words.append("low")
+    words.append("lower")
+    words.append("lowest")
+
+    bpe = ByteLevelBPE(3)
+
+    bpe.train(words)
+
+    ids_before = bpe.encode_to_ids("lowest")
+
+    bpe.save_merges("merges.json")
+    bpe.save_vocab("vocab.json")
+
+    bpe2 = ByteLevelBPE(3)
+
+    bpe2.load_merges("merges.json")
+    bpe2.load_vocab("vocab.json")
+
+    ids_after = bpe2.encode_to_ids("lowest")
+
+    assert ids_before.to_list() == ids_after.to_list()
+
+
+# Decode round trip
+def test_decode_roundtrip():
+    words = DynamicArray()
+
+    words.append("low")
+    words.append("lower")
+    words.append("lowest")
+
+    bpe = ByteLevelBPE(3)
+
+    bpe.train(words)
+
+    text = "lowest"
+
+    ids = bpe.encode_to_ids(text)
+
+    decoded = bpe.decode_ids(ids)
+
+    assert decoded == text
+
+
+# Unicode
+def test_unicode():
+    words = DynamicArray()
+
+    words.append("low")
+    words.append("lower")
+    words.append("lowest")
+
+    bpe = ByteLevelBPE(3)
+
+    bpe.train(words)
+
+    text = "hello 🚀"
+
+    ids = bpe.encode_to_ids(text)
+
+    decoded = bpe.decode_ids(ids)
+
+    assert decoded == text
